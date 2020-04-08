9e26d490e3a8c070d0406089553376109a47f09d
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2020-04-08 13:34:23 -0400

    Update Cloud_Jump.py
    Checking the array recursion

--- Cloud_Jump.py
+++ Cloud_Jump.py
@@ -1,28 +1,7 @@
 def jumpingOnClouds(c):
-    start = jumps = 0
-    traverse = len(c) - 1
-    while start <= traverse:
-        try:
-            if c[start + 2] == 0:
-                jumps += 1
-                start += 2
-                print(jumps, start)
-            else:
-                jumps += 1
-                start += 1
-                print(jumps, start)
-        except:
-                jumps += 1
-                start += 1
-                print(jumps, start)
-    return(jumps)
+    for i in range(c):
+        print((i+3)%c)
 
 if __name__ == '__main__':
-
-    n = int(input())
-
-    c = list(map(int, input().rstrip().split()))
-
-    result = jumpingOnClouds(c)
-
-    print(str(result) + '\n')
+    c = 6
+    jumpingOnClouds(c)
