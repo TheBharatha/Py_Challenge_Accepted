ea48391863191b4df33be013ac7a59cfef933c7a
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2020-04-08 21:47:45 -0400

    Update Cloud_Jump.py
    Found the solution.
    At first, I thought of solving this with two for loops and then coudn't figure when the index turns 0. So, a for loop within the while works fine.

--- Cloud_Jump.py
+++ Cloud_Jump.py
@@ -1,7 +1,20 @@
-def jumpingOnClouds(c):
-    for i in range(c):
-        print((i+3)%c)
+def jumpingOnClouds(c, k):
+    stop = False
+    energy = 100
+    index = 0
+    while stop == False:
+        for _ in range(k):
+            energy -= 1
+            index = (index + k)%len(c)
+            if c[index] == 1:
+                energy -= 2
+            print('Index: ',index, 'Energy: ', energy)
+            if index == 0:
+                stop = True
+                break
+    print(energy)
 
 if __name__ == '__main__':
-    c = 6
-    jumpingOnClouds(c)
+    c = [0,0,1,0,0,1,1,0]
+    k = 2
+    jumpingOnClouds(c, k)
