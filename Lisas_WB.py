78353d891a49dc3be972c1d2842644229d3b2ff3
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2020-04-09 19:05:25 -0400

    HackerRank, Lisa's workbook solution

--- Lisas_WB.py
+++ Lisas_WB.py
@@ -1,7 +1,25 @@
 import math
 n = 5
 k = 3
-arr = [4,2,6,1,10]
+arr = [4,2]
+specialProblems = startPage = 0
+currentPage = problemNo = 1
+for chapter in arr:
+    numberOfPages = math.ceil(chapter/k)
+    while problemNo <= chapter:
+        #print('Page no', currentPage, 'Problem No: ', problemNo)
+        if currentPage == problemNo:
+            specialProblems += 1
+        if problemNo % k == 0:
+            currentPage += 1
+        problemNo += 1
+    if currentPage == startPage + numberOfPages:
+        currentPage += 1
+    startPage = currentPage - 1
+    problemNo = 1
+print(specialProblems)
+    
+'''
 startPage = 0
 specialProblems = 0
 problemNo = 1
@@ -16,8 +34,9 @@ while chapter <= n:
             if problemNo % k == 0:
                 currentPage  += 1
                 print('Turned to page: ', currentPage)
-            problemNo += 1
+                problemNo += 1
         currentPage += 1
     problemNo = 1
     startPage = currentPage - 1
     chapter += 1
+'''
