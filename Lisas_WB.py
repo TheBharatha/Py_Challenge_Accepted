d76227b56843e3e6647f2eea6ba4c2474bfe0e38
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2020-04-09 19:26:14 -0400

    HackerRank, Lisa's workbook solution explained

--- Lisas_WB.py
+++ Lisas_WB.py
@@ -1,42 +1,39 @@
 import math
-n = 5
-k = 3
-arr = [4,2]
-specialProblems = startPage = 0
-currentPage = problemNo = 1
-for chapter in arr:
-    numberOfPages = math.ceil(chapter/k)
-    while problemNo <= chapter:
-        #print('Page no', currentPage, 'Problem No: ', problemNo)
-        if currentPage == problemNo:
-            specialProblems += 1
-        if problemNo % k == 0:
-            currentPage += 1
-        problemNo += 1
-    if currentPage == startPage + numberOfPages:
-        currentPage += 1
-    startPage = currentPage - 1
-    problemNo = 1
-print(specialProblems)
-    
-'''
-startPage = 0
-specialProblems = 0
-problemNo = 1
-chapter = 1
-currentPage = 1
-while chapter <= n:
-    print('Chapter: ', chapter)
-    numberOfPages = math.ceil(arr[chapter-1]/k)
-    while currentPage <= startPage + numberOfPages:
-        while problemNo <= arr[chapter-1]:
-            print('Page no:', currentPage, 'Problem No: ', problemNo)
+
+def workbook(n, k, arr):
+    chapterCount = specialProblems = startPage = 0
+    currentPage = problemNo = 1
+    for chapter in arr:
+        chapterCount += 1
+        print('\n Chapter: ', chapterCount)
+        numberOfPages = math.ceil(chapter/k)
+        print('Number of pages required for ', chapter, 'problem/s are/is ', numberOfPages)
+        while problemNo <= chapter:
+            print('Page no: ', currentPage, ', Problem index: ', problemNo)
+            if currentPage == problemNo:
+                print('Special problem match found at page ', currentPage, 'for problem ', problemNo)
+                specialProblems += 1
             if problemNo % k == 0:
-                currentPage  += 1
-                print('Turned to page: ', currentPage)
-                problemNo += 1
-        currentPage += 1
-    problemNo = 1
-    startPage = currentPage - 1
-    chapter += 1
-'''
+                currentPage += 1
+                print('Max number of problems per page reached! moving to page', currentPage)
+            problemNo += 1
+        if currentPage == startPage + numberOfPages:
+            currentPage += 1
+            print('Visited all the problems in chapter', chapterCount, 'moving to page', currentPage)
+        startPage = currentPage - 1
+        problemNo = 1
+    return(specialProblems)
+
+if __name__ == '__main__':
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = workbook(n, k, arr)
+
+    print('\n Total number of special problems in the workbook are ', str(result))
