eb4b7fe1b8a028941632bf745cafbd3510419111
BharathKumarS <bskumar1@outlook.com>
2020-04-29 22:29:06 -0400

    Amazon SIP, Run length Encoding. Brute force approach

--- RunningLen.py
+++ RunningLen.py
@@ -4,24 +4,24 @@ def stringComprress(givenStr):
     afterCompression = ''
     # Assuming there is at least one character in the given string
     charCount = 1
-    # Looping through all the charecters in the given string
+    # Looping through all the characters in the given string
     # String with index 1 through the length of the given string
     for char in range(1, len(givenStr)):
         # Condition to check if the current string is same as previous string
         if givenStr[char-1] == givenStr[char]:
-            # If the current charecter is same as the previous charecter?, variable charCount will increment by 1
+            # If the current character is same as the previous character?, variable charCount will increment by 1
             charCount += 1
-        # Else condition. If the given charecter is not same as the previous charecter
+        # Else condition. If the given character is not same as the previous character
         else:
-            # Variable afterCompression is concatenated with the variable charCount and the previous charecter
+            # Variable afterCompression is concatenated with the variable charCount and the previous character
             afterCompression = afterCompression + str(charCount) + givenStr[char-1]
             # Reetting the variable charCount as we have reached the end of similar characters
             charCount = 1
-    # Within the above for loop, the last set of similar charecters are skipped because the else condition will not be satisfied
-    # Try block will make sure to add charCount and the current charecter to the string variable afterCompression
+    # Within the above for loop, the last set of similar characters are skipped because the else condition will not be satisfied
+    # Try block will make sure to add charCount and the current character to the string variable afterCompression
     try:
         afterCompression = afterCompression + str(charCount) + givenStr[char]
-    # Except block will handle Border case inputs; If there is only one charecter in the given string
+    # Except block will handle Border case inputs; If there is only one character in the given string
     except:
         afterCompression = afterCompression + str(charCount) + givenStr
     # Return afterCompression to the calling function
