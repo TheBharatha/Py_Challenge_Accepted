e15cb5ed70ff10b010b6c454c0620e77797c29e9
Bharath Kumar Shivakumar <bskumar1@outlook.com>
2020-05-14 21:13:05 -0400

    HackerRank, Roman to Integer
    Changed the irregular variable declaration of the dictionary.

--- I_Can_Handel_Romans.py
+++ I_Can_Handel_Romans.py
@@ -1,12 +1,12 @@
 def roam(s):
-    Guide = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D': 500, 'M':1000}
-    num = prev_char = 0
+    guide = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D': 500, 'M':1000}
+    value = prev_char = 0
     for char in s[::-1]:
-        if Guide[char] >= prev_char:
-            prev_char = Guide[char]
-            num = num + Guide[char]
+        if guide[char] >= prev_char:
+            prev_char = guide[char]
+            value = value + guide[char]
         else:
-            num = num - Guide[char]
-    print(num)
+            value = value - guide[char]
+    print(value)
     
 roam('MCMXCIV')   
