4fac02883670fbefdd00ab3fb3b2591bc509ed33
BharathKumarS <bskumar1@outlook.com>
2020-05-16 19:47:44 -0400

    LeetCode, Odd Even Linked List

--- Group_LinkedList.py
+++ Group_LinkedList.py
@@ -1,7 +1,8 @@
- class ListNode:
-     def __init__(self, val=0, next=None):
-         self.val = val
-         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
 class Solution:
     def __init__(self):
         self.oddNodes = list()
