0006d30712f42f6014b8d5eb18fafe2eca04c176
BharathKumarS <bskumar1@outlook.com>
2020-05-16 19:46:09 -0400

    LeetCode, Odd Even Linked List

---
+++ Group_LinkedList.py
@@ -0,0 +1,34 @@
+ class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+class Solution:
+    def __init__(self):
+        self.oddNodes = list()
+        self.evenNodes = list()
+    
+        
+    def oddEvenList(self, head: ListNode) -> ListNode:
+        nodeValue = 1
+        while head:
+            if nodeValue%2 == 0:
+                self.evenNodes.append(head.val)
+                head = head.next
+            else:
+                self.oddNodes.append(head.val)
+                head = head.next
+            nodeValue += 1
+            
+        self.oddNodes.extend(self.evenNodes)
+        
+        for nodes in self.oddNodes:
+            newNode = ListNode(nodes)
+            if head:
+                current = head
+                while current.next:
+                    current = current.next
+                current.next = newNode
+            else:
+                head = newNode
+                
+        return head
