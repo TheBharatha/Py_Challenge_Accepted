319a43e2e302dad8ab13f56f0cd5ec2296f4db8d
BharathKumarS <bskumar1@outlook.com>
2020-05-24 12:52:12 -0400

    LeetCode, COnstruct BST from preorder traversal

---
+++ BST from PreOrder.py
@@ -0,0 +1,11 @@
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        if preorder:
+            return TreeNode(preorder[0],
+                           self.bstFromPreorder(tuple(takewhile(lambda x:x<preorder[0], preorder[1:]))),
+                           self.bstFromPreorder(tuple(dropwhile(lambda x:x<preorder[0], preorder[1:]))))
