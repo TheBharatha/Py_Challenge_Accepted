8ff1a16b720ba6d7458f12de157c67f7a11e318d
BharathKumarS <bskumar1@outlook.com>
2020-05-26 10:15:00 -0400

    LeetCode, Binary Search Tree from PreOrder traversal

--- BST from PreOrder.py
+++ BST from PreOrder.py
@@ -1,10 +1,12 @@
+from itertools import takewhile
+from itertools import dropwhile
 class TreeNode:
      def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 class Solution:
-    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+    def bstFromPreorder(self, preorder):
         if preorder:
             return TreeNode(preorder[0],
                            self.bstFromPreorder(tuple(takewhile(lambda x:x<preorder[0], preorder[1:]))),
