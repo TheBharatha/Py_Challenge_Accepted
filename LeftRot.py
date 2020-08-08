1342440aa7f6e09f38a27bb6a7eb9a8dcd2ef700
BharathKumarS <bskumar1@outlook.com>
2020-08-07 22:52:39 -0500

    Update LeftRot.py
    No print, but return

--- LeftRot.py
+++ LeftRot.py
@@ -3,12 +3,12 @@ def rotLeft(a, d):
     partOne = a[:d]
     a = a[d:]
     a.extend(partOne)
-    print(a)
+    return(a)
 
 if __name__ == '__main__':
     nd = input().split()
     n = int(nd[0])
     d = int(nd[1])
     a = list(map(int, input().rstrip().split()))
-    result = rotLeft(a, d)
+    result = rotLeft(a,d)
     print(' '.join(map(str, result)))
